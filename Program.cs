83dfa0a52bf3361313d4474ab409eb9f0d630d19
manju12345 <manju@gmail.com>
2023-07-22 17:31:02 +0530

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+
+namespace AgeValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            bool isValidInput = false;
+
+            while (!isValidInput)
+            {
+                Console.Write("Please enter your age: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out age))
+                {
+                    if (age >= 0)
+                    {
+                        isValidInput = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Age cannot be negative.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid age (an integer).");
+                }
+            }
+
+            if (age < 18)
+            {
+                Console.WriteLine("You are a minor.");
+            }
+            else if (age >= 18 && age < 65)
+            {
+                Console.WriteLine("You are an adult.");
+            }
+            else
+            {
+                Console.WriteLine("You are a senior.");
+            }
+        }
+    }
+}
